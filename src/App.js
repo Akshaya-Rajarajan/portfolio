e21d7c7d866fec6f92643f2892992e9be2ce9725
Akshaya Rajarajan <akshaya.rajarajan.96@gmail.com>
2024-08-01 00:22:54 -0500

    Designed the footer and contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Resume from './components/Resume';
 
 
 function App() {
   return (
       <div className="App">
           <Header/>
+          <Resume/>
+          <Contact/>
+          <Footer/>
+
       </div>
   );
 }
