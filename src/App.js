fffbdfd243715319d9f94b84d40c1ec9077b46c3
Akshaya Rajarajan <akshaya.rajarajan.96@gmail.com>
2024-08-01 14:19:00 -0500

    Projects and Resume section drafted

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 import Resume from './components/Resume';
 
 
@@ -14,6 +15,7 @@ function App() {
           <Header/>
           <Hero/>
           <About/>
+          <Projects/>
           <Resume/>
           <Contact/>
           <Footer/>
